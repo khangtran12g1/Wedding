b017640d01eb40114c355070d756991a71cd0b28
khangtran12-g1 <2124802010094@student.tdmu.edu.vn>
2025-06-19 15:52:15 +0700

    fix: sửa lỗi scroll khi đổi trang

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import Layout from "./User/components/Layout";
 import Home from "./User/pages/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import TrangTriGt from "./User/pages/Trang_Tri_GT";
 import Product from "./User/pages/Product";
+import ScrollToTop from "./User/components/ScrollToTop"; // hoặc đúng đường dẫn
+
 
 function App() {
   return (
     <Router>
+      <ScrollToTop/>
       <Layout>
         <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
           <Route path="/product" element={< Product/>} />
           <Route path="/trangtrigt" element={< TrangTriGt/>} />
           <Route path="/home" element={< Home/>} />
