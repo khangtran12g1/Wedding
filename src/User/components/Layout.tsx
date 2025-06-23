17aeb9bac48bac5b18c6557bc626fec47905d579
khangtran12-g1 <2124802010094@student.tdmu.edu.vn>
2025-06-24 01:41:56 +0700

    s

--- src/User/components/Layout.tsx
+++ src/User/components/Layout.tsx
@@ -1,11 +1,12 @@
 import Header from "./Header";
 import Footer from "./Footer";
-
+import ToolLeft from "./ToolLeft";
 function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
       <main className="flex-1 bg-white">{children}</main>
+      <ToolLeft/>
       <Footer />
     </div>
   );
